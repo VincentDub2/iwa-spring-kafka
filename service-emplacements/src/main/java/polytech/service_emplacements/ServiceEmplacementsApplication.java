4ee279d443c2df227b52b2573d8440a1ddf4b31a
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-10-30 12:33:14 +0100

    -init project

---
+++ service-emplacements/src/main/java/polytech/service_emplacements/ServiceEmplacementsApplication.java
@@ -0,0 +1,13 @@
+package polytech.service_emplacements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmplacementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmplacementsApplication.class, args);
+	}
+
+}
