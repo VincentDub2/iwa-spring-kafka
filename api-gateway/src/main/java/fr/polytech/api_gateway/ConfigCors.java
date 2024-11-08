ff7b4695ffc4bf052d657ff31c02f2ccb5386fcd
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-08 10:04:22 +0100

    Set up microservice gateway

---
+++ api-gateway/src/main/java/fr/polytech/api_gateway/ConfigCors.java
@@ -0,0 +1,32 @@
+package fr.polytech.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class ConfigCors {
+
+    /**
+     * Configure the CORS filter to allow requests from the Expo app
+     *
+     * @return CorsWebFilter object to be used by Spring Security
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        //corsConfig.addAllowedOrigin("http://localhost:19006");
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+
+}
