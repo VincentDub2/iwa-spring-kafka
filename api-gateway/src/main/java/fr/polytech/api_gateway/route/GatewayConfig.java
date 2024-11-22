4459cefeab951e086d7e46e24d0397fc6ef99141
Alexandre Lagorce <alexandrelagorce@MacBook-Pro-de-Alexandre.local>
2024-11-22 15:51:40 +0100

    add attribute phone

--- api-gateway/src/main/java/fr/polytech/api_gateway/route/GatewayConfig.java
+++ api-gateway/src/main/java/fr/polytech/api_gateway/route/GatewayConfig.java
@@ -1,11 +1,11 @@
 package fr.polytech.api_gateway.route;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @Configuration
